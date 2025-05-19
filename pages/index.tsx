2ceaa0fbcc8cab3812ee68ed9a0ef2fcb4cc5748
Ana <ana.vega@udea.edu.co>
2025-05-19 17:14:33 -0500

    Prueba

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
   return (
+    <section>
+      hola mundo
+
+
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <LoginForm />
       </div>
     </div>
+    </section>
   )
 }

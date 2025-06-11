986591327daad9bb0233e6b6caf69d6aabee2658
Ana <ana.vega@udea.edu.co>
2025-06-11 16:45:44 -0500

    Arreglos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import { LoginForm } from "@/components/login-form"
+import React from 'react';
+import { LoginForm } from '@/components/login-form';
 
-export default function LoginPage() {
+export default function Home() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm />
-      </div>
+    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+      <LoginForm />
     </div>
-  )
+  );
 }
